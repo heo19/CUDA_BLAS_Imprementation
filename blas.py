77649f5e861b35b96a3687da9193466b19ed6f1c
Hyeonwoo Heo <hyeonwoo.heo@gmail.com>
2024-05-28 11:05:38 +0900

    Added fuction that can takes kernel as parameter and execute the kernel

--- blas.py
+++ blas.py
@@ -13,18 +13,7 @@ def ASSERT_DRV(err):
         raise RuntimeError("Unknown error type: {}".format(err))
 
 
-negation = """\
-extern "C" __global__
-void neg(float *a, float *b, size_t n)
-{
-  size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
-  if (tid < n) {
-    b[tid] = -a[tid];
-  }
-}
-"""
-
-addition = """\
+add = """\
 extern "C" __global__
 void add(float *a, float *b, float *c, size_t n)
 {
@@ -35,7 +24,7 @@ void add(float *a, float *b, float *c, size_t n)
 }
 """
 
-subtraction = """\
+sub = """\
 extern "C" __global__
 void sub(float *a, float *b, float *c, size_t n)
 {
@@ -46,7 +35,7 @@ void sub(float *a, float *b, float *c, size_t n)
 } 
 """
 
-multiplication = """\
+mul = """\
 extern "C" __global__
 void mul(float *a, float *b, float *c, size_t n)
 {
@@ -57,3 +46,105 @@ void mul(float *a, float *b, float *c, size_t n)
 }    
 """
 
+
+def execute_kernel(kernel_code, kernel_name):
+    NUM_THREADS = 512  # Threads per block
+    NUM_BLOCKS = 32768  # Blocks per grid
+
+    temp = np.array([2.0], dtype=np.float32)
+    n = np.array(NUM_THREADS * NUM_BLOCKS, dtype=np.uint32)
+    bufferSize = n * (temp).itemsize
+
+    a = np.random.rand(n).astype(dtype=np.float32)
+    b = np.random.rand(n).astype(dtype=np.float32)
+    out = np.zeros(n).astype(dtype=np.float32)
+
+    print("Kernel to Execute: ", kernel_code)
+    print("Vector A: ", a)
+    print("Vector B: ", b)
+    print("Vector Out: ", out)
+    print("Size of Vector: ", n)
+    
+    #Create program
+    err, prog = nvrtc.nvrtcCreateProgram(str.encode(kernel_code), str.encode(f"{kernel_name}.cu"), 0, [], [])
+    
+    #compile program
+    opts = [b"--fmad=false", b"--gpu-architecture=compute_75"]
+    err, = nvrtc.nvrtcCompileProgram(prog, 2, opts)
+    
+    #Get PTX
+    err, ptxSize = nvrtc.nvrtcGetPTXSize(prog)
+    ptx = b" " * ptxSize
+    err, = nvrtc.nvrtcGetPTX(prog, ptx)
+    
+    #Initialize CUDA Driver API
+    err, = cuda.cuInit(0)
+    
+    # Retrieve handle for device 0
+    err, cuDevice = cuda.cuDeviceGet(0)
+
+    # Create context
+    err, context = cuda.cuCtxCreate(0, cuDevice)
+
+    # Load PTX as module data and retrieve function
+    ptx = np.char.array(ptx)
+    # Note: Incompatible --gpu-architecture would be detected here
+    err, module = cuda.cuModuleLoadData(ptx.ctypes.data)
+    ASSERT_DRV(err)
+    err, kernel = cuda.cuModuleGetFunction(module, str.encode(f"{kernel_name}"))
+    ASSERT_DRV(err)
+    
+    err, dAclass = cuda.cuMemAlloc(bufferSize)
+    err, dBclass = cuda.cuMemAlloc(bufferSize)
+    err, dOutclass = cuda.cuMemAlloc(bufferSize)
+
+    err, stream = cuda.cuStreamCreate(0)
+
+    err, = cuda.cuMemcpyHtoDAsync(
+        dAclass, a.ctypes.data, bufferSize, stream
+    )
+    err, = cuda.cuMemcpyHtoDAsync(
+        dBclass, b.ctypes.data, bufferSize, stream
+    )
+    
+    # The following code example is not intuitive 
+    # Subject to change in a future release
+    dA = np.array([int(dAclass)], dtype=np.uint64)
+    dB = np.array([int(dBclass)], dtype=np.uint64)
+    dOut = np.array([int(dOutclass)], dtype=np.uint64)
+
+    args = [dA, dB, dOut, n]
+    args = np.array([arg.ctypes.data for arg in args], dtype=np.uint64)
+
+    err, = cuda.cuLaunchKernel(
+        kernel,
+        NUM_BLOCKS,  # grid x dim
+        1,  # grid y dim
+        1,  # grid z dim
+        NUM_THREADS,  # block x dim
+        1,  # block y dim
+        1,  # block z dim
+        0,  # dynamic shared memory
+        stream,  # stream
+        args.ctypes.data,  # kernel arguments
+        0,  # extra (ignore)
+    )
+
+    err, = cuda.cuMemcpyDtoHAsync(
+    out.ctypes.data, dOutclass, bufferSize, stream
+    )
+    err, = cuda.cuStreamSynchronize(stream)
+
+    # Assert values are same after running kernel
+    print("Vector Out After Kernel Execution: ", out)
+    
+    err, = cuda.cuStreamDestroy(stream)
+    err, = cuda.cuMemFree(dAclass)
+    err, = cuda.cuMemFree(dBclass)
+    err, = cuda.cuMemFree(dOutclass)
+    err, = cuda.cuModuleUnload(module)
+    err, = cuda.cuCtxDestroy(context)
+
+execute_kernel(add, "add")
+execute_kernel(sub, "sub")
+execute_kernel(mul, "mul")
